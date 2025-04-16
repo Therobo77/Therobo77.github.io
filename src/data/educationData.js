8bd3c77f247f1242158c618d29e1ec1c771ff773
Vikash Kumar Lal <vikashkumar.l@connectindia.com>
2025-04-16 16:04:43 +0530

    education

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,24 +1,24 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Masai School Bangalore',
-        course: 'Full Stack Web-developement (Part-Time)',
-        startYear: 'March 2022',
-        endYear: 'Feb 2023'
+        institution: 'Indian Institute of Technology, Kharagpur ',
+        course: 'Internet of Things (IoT) Certification,IoT Engineering ',
+        startYear: ' 2024',
+        endYear: '2025'
     },
-    {
-        id: 2,
-        institution: 'Radha Govind University , Jharkhand',
-        course: 'B.Sc General Science',
-        startYear: '2019',
-        endYear: '2022'
-    },
-    {
-        id: 3,
-        institution: 'Deoghar College Deoghar , Jharkhand',
-        course: 'Intermediate of Science',
-        startYear: '2016',
-        endYear: '2018'
-    },
+    // {
+    //     id: 2,
+    //     institution: 'Radha Govind University , Jharkhand',
+    //     course: 'B.Sc General Science',
+    //     startYear: '2019',
+    //     endYear: '2022'
+    // },
+    // {
+    //     id: 3,
+    //     institution: 'Deoghar College Deoghar , Jharkhand',
+    //     course: 'Intermediate of Science',
+    //     startYear: '2016',
+    //     endYear: '2018'
+    // },
 ]
 
